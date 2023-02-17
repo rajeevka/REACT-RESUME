a8e3588267c4e42106af790f19ec5fe385981264
rajeevka <rke39412@gmail.com>
2023-02-17 16:06:02 +0530

    geekresume

---
+++ geekresume/src/App.js
@@ -0,0 +1,15 @@
+
+import Resume from "./Resume";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Resume/>
+    </div>
+  );
+}
+
+export default App;
